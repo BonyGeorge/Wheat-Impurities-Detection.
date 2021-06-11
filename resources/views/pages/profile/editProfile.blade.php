043d14c210760478e86e066181b5c687ac26a5f7
nourbahaa461 <57958235+nourbahaa461@users.noreply.github.com>
2021-06-11 19:41:31 +0200

    edit profile

--- resources/views/pages/profile/editProfile.blade.php
+++ resources/views/pages/profile/editProfile.blade.php
@@ -1,148 +1,137 @@
-
 <!DOCTYPE html>
+@extends('layouts.sidebar')
+
+@section('content')
+
 <html>
-<head>
+<title> Wheat System | Edit Profile.</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wheat System | Edit Profile.</title>
     <link rel="icon" href="{{asset('Logo.png')}}">
-
-
 <style>
-body {
-    font-family: Arial, Helvetica, sans-serif;
-    }
-* {box-sizing: 
-border-box;}
-
-.input-container {
-  display: -ms-flexbox; /* IE10 */
-  display: flex;
-  width: 100%;
-  margin-bottom: 20px;
-  margin-left: 100px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 300px;
-
-}
-
-.icon {
-  padding: 10px;
-  background-color:  #B38728;  
-  color: white;
-  min-width: 50px;
-  border-radius: 4px;
-  text-align: center;
-  margin-right: 7px;
-}
-
-.input-field {
-  width: 100%;
-  padding: 10px;
-}
-
-
-.btn {
-  background-color: #B38728; 
-  color: white;
-  padding: 10px ;
-  margin-top:30px ;
-  margin-left: 80px;
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+<style>
+.button {
+  display: inline-block;
+  border-radius: 40px;
+  background-color:goldenrod;
   border: none;
-  width: 70%;
-  border-radius: 12px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  opacity: 0.9;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 20px;
+  padding: 20px;
+  width: 150px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+  margin-left: 300px;
 }
-body{
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: 100% 100%;
+
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
 }
-form{
-    background-color: rgb(231, 235, 225);
-    font-size: x-large;
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
 }
-fieldset{
-    margin: 20px;
+
+.button:hover span {
+  padding-right: 25px;
 }
-img{
-   margin-left: 150px;
-   width:150px;
-   height:150px;   
-   border-radius: 10000px;   
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
 }
 
 </style>
+<body class="w3-light-grey ">
 
+<!-- Page Container -->
+<div class="w3-content w3-margin-top " style="max-width:1100px;">
 
-</head>
-<body style="
-background-image: url({{asset('wheat.jpg')}})">
-    @extends('layouts.sidebar')
-    
-    @section('content')
-
-<form method="POST" action="/profile/{{Auth::user()->id}}" enctype="multipart/form-data" style="max-width:500px;margin:auto" >
-    @csrf
-    @method('PATCH')
-    <fieldset>
-        <legend>Profile Page</legend>
-
-
-    <div style="align-self: ">
-    @if(Auth::user()->filename == NULL)
-    <img src="{{asset('profile.png')}}">
-
-    @else
-    
-    <img src="{{asset('storage/ProfilePicture/' .Auth::user()->filename)}}"  alt="Image">
-    @endif
-    </div> 
-
-  <div class="input-container">
-    <i class="fa fa-user icon"></i>
-    <input class="input-field" type="text" name="name" value="{{Auth::user()->name}}" id="usrnm">
-  </div>
-
-  <div class="input-container">
-    <i class="fa fa-envelope icon"></i>
-    <input class="input-field" type="email" name="email" value="{{Auth::user()->email}}" id="email">
-  </div>
-
-  <div class="input-container">
-    <i class="fas fa-home icon"></i>
-    <input class="input-field" type="email" name="address" value="{{Auth::user()->address}}" id="email">
-  </div>
-
-  <div class="input-container">
-    <i class="fa fa-phone icon"></i>
-    <input class= "number" type="number" name="phone" value="{{Auth::user()->phone}}" id="zipcode-number" maxlength="11" min="0" max="99999999999" style="  width: 100%; padding: 10px;">
-  </div>
-
+  <!-- The Grid -->
+  <div class="w3-row-padding ">
   
+    <!-- Left Column -->
+    <div class="w3-col m5 w3-center"  >
+    
+      <div class="w3-white w3-text-grey w3-card-4 ">
+        <div class="w3-display-container ">
+        @if(Auth::user()->filename == NULL)
+          <img src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg" style="width:100%" alt="Avatar">
+        @else
 
-  <div class= "input-container">
-      <i class ="fa icon">&#xf224;</i>
-      <select name="isMale" id="gen" >
+        <img src="{{asset('storage/ProfilePicture/'.Auth::user()->filename)}}"  alt="User Image" / width="70cm" height="70cm">
+
+        @endif
+
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+            <h2> {{Auth::user()->name}}</h2>
+          </div>
+        </div>
+        <br>
+        <div class="w3-container ">
+        <h6 class="w3-text-yellow"><i class="fa fa-envelope icon "></i>
+    <input class="input-field" type="email" name="email" value="{{Auth::user()->email}}" id="email"style="  width: 60%; margin:10px; ">
+    <h6 class="w3-text-yellow"> <i class="fas fa-home icon"></i>
+    <input class="input-field" type="email" name="address" value="{{Auth::user()->address}}" id="email"style="  width: 60%; margin:10px; ">
+   <br> <h6 class="w3-text-yellow"><i class="fa fa-phone icon"></i>
+    <input class= "number" type="number" name="phone" value="{{Auth::user()->phone}}" id="zipcode-number" maxlength="11" min="0" max="99999999999" style="  width: 60%; margin:10px; ">
+   <br>
+   <h6 class="w3-text-yellow"> <i class="fa fa-upload icon"></i>
+    <input type="file" name="filename" style="  width: 60%; margin:10px; "></div>
+    
+    <h6 class="w3-text-yellow"><i class ="fa fa-transgender icon"  ></i>
+      <select name="isMale" id="gen" style="  width: 40%; margin:10px; ">
         <option value="1">Male</option>
         <option value="0">Female</option>      
       </select>
-      </div>
+       
+       </div>
+      </div><br> 
+    
 
-  <div class="input-container">
-    <i class="fa fa-upload icon"></i>
-    <input type="file" name="filename" ></div>
+  
+  
+      <input type="submit" name="Save Edits" value="Save Edits" class = "button" style="vertical-align:center ; margin-top:300px;">
+    
+    </div>
+    
+  
+  </div>
+  
+ 
+  </div>
+                       
 
-    <input type="submit" name="Save Edits" value="Save Edits" class = "btn btn-warning">
-</fieldset>
-</form>
-</div>
-</div>
-<footer>
-        @include('layouts.footer')
-</footer>
-    @endsection
 </body>
 </html>
+ <!-- jQuery -->
+ <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="{{asset('js/popper.min.js')}}"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="{{asset('js/mdb.min.js')}}"></script>
+
+  <footer id="footer">
+  @include('layouts.footer')
+</footer>
+@endsection
