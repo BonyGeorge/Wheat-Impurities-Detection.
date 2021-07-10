a4f269a0c32be2bccb86ae6b830a79ee1ba705cf
Ibrahim Fawzy <62511760+fawzyibra@users.noreply.github.com>
2021-07-10 14:45:31 +0200

    nav bar updates

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -55,11 +55,11 @@
           <!-- Authentication Links -->
           @guest
               <li class="nav-item">
-                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  <a class="nav-link" href="{{ route('login') }}"> @lang('lang.login') </a>
               </li>
               @if (Route::has('register'))
                   <li class="nav-item">
-                      <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                      <a class="nav-link" href="{{ route('register') }}"> @lang('lang.reg') </a>
                   </li>
               @endif
           @else
