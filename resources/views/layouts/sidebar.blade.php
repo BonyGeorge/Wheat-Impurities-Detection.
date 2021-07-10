59b13b62cc067197a2323f3d64c6d4311f2ef732
Ibrahim Fawzy <62511760+fawzyibra@users.noreply.github.com>
2021-07-10 15:04:58 +0200

    updates admin

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -40,7 +40,7 @@
           
           onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
-           {{ __('Logout')  }}
+            @lang('lang.logout') 
        </a>
        
 
