c05db24ee9d76c5708f190f9bb8848b7870d2b82
Ibrahim Fawzy <62511760+fawzyibra@users.noreply.github.com>
2021-07-10 14:49:27 +0200

    some updates in welcom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <html lang="en">
 
 <head>
-            <title>@lang('lang.welcome')</title>
+            <title>@lang('lang.Start')</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta charset="UTF-8" />
 
