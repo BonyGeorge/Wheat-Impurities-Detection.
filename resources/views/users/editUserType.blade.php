f67ac1247473ce013609ead66a422c41b3088f7a
Ibrahim Fawzy <62511760+fawzyibra@users.noreply.github.com>
2021-07-13 15:52:04 +0200

    final edits arabic

--- resources/views/users/editUserType.blade.php
+++ resources/views/users/editUserType.blade.php
@@ -1,24 +1,128 @@
+@extends('layouts.sidebar')
+
+@section('content')
 <!-- Modal -->
+<!DOCTYPE html>
+<html>
 <head>
     <meta charset="UTF-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <title> @lang('lang.editusertypetitle') </title>
-       <link rel="icon" href="favicon.ico">
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
-  <!-- Google Fonts Roboto -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-  <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-  <!-- Material Design Bootstrap -->
-  <link rel="stylesheet" href="{{asset('css/mdb.min.css')}}">
-  <!-- Your custom styles (optional) -->
-  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@lang('lang.editusertypetitle')</title>
+    <link rel="icon" href="Logo.png">
+    <style>
+        
+   #mailone {
+        background-color: #3bde40;
+        position: relative;
+        color: black;
+        height: 30px;
+        border: 1px solid transparent;
+        border-color: transparent transparent rgba(0, 0, 0, 0.1) transparent;
+        cursor: pointer;
+        user-select: none;
+        text-color: black;
+    }
 
+    #mailone-text {
+        color: black;
+    }
 
-       </style>
-    </head>
+    #mcontent,
+    #msubject {
+        width: 100%;
+
+    }
+
+    #tarea {
+        position: relative;
+        left: 25%;
+        display: flex;
+        width: 50%;
+    }
+
+    #tareaa {
+        position: relative;
+        left: 25%;
+        display: flex;
+        width:
+            50%;
+
+    }
+
+    #mcontent {
+        border: 2px solid gold;
+        -webkit-border-radius: 5px;
+        -moz-border-radius: 5px;
+        border-radius: 5px;
+        padding: 4px;
+    }
+
+    #msubject {
+        border: 2px solid gold;
+        -webkit-border-radius: 5px;
+        -moz-border-radius: 5px;
+        border-radius: 5px;
+        padding: 4px;
+    }
+
+    .mbutton {
+        display: inline-block;
+  border-radius: 40px;
+  background-color:goldenrod;
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 20px;
+  padding: 20px;
+  width: 150px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+  margin-left: 450px;
+    }
+
+.mbutton span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.mbutton span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.mbutton:hover span {
+  padding-right: 25px;
+}
+
+.mbutton:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+    .mailer {
+        position: relative;
+        left: 44.2%
+    }
+
+    @media only screen and (max-width: 850px) {
+        .mbutton {
+            left: 27.5%;
+        }
+
+        .mailer {
+            position: relative;
+            left: 31.2%;
+        }
+    } </style>
+</head>
    
-
+<body>
 
       <div class="modal-header text-center">
         <h3 class="modal-title w-100 dark-grey-text font-weight-bold my-3" id="myModalLabel"><strong>@lang('lang.editusertypetittle2')</strong></h3>
@@ -63,6 +167,7 @@
             {!! Form::submit("Change", ['class' => 'btn blue-gradient btn-block btn-rounded z-depth-1a']) !!}
           </div>
       </div>
+    </body>
       
       <!--Footer-->
     </div>
